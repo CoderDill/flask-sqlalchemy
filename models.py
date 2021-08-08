c2fe8c6cf117c6a0e3b7e45d68a8f5c2047447e7
CrappyDill <mddillon47@gmail.com>
2021-08-08 14:07:10 -0700

    first model

--- models.py
+++ models.py
@@ -1,7 +1,20 @@
+from os import name
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+    """Pet."""
+
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    species = db.Column(db.String(30))
+    hunger = db.Column(db.Integer, nullable=False, default=20)
