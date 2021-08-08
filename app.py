00974f9fa8399654069229b88d7040256dfcf49d
CrappyDill <mddillon47@gmail.com>
2021-08-08 13:40:29 -0700

    sqlalchemy boilerplater

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from flask_sqlalchemy import SQLAlchemy
+from models import db, connect_db
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
-
-db = SQLAlchemy()
-db.app = app
-db.init_app(app)
-
-app.config["SECRET_KEY"] = 'coolbeans'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'coolbeans'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+connect_db(app)
 
 
 @app.route("/")
