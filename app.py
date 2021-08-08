a856fb8eb76d3c344b3b27e9a98e74f519e73261
CrappyDill <mddillon47@gmail.com>
2021-08-08 13:25:23 -0700

    db connected

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example'
+
+db = SQLAlchemy()
+db.app = app
+db.init_app(app)
+
 app.config["SECRET_KEY"] = 'coolbeans'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
 
 @app.route("/")
 def home_page():
-    
+
     return render_template("home.html")
